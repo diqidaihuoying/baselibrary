e2e8f657874c1f8c987e21afc4236f26663c2e4e
wantao <wantao@cappu.com>
2018-06-29 15:08:37 +0800

    详情界面的

--- baseLibrary/src/main/java/cn/com/base/simple/bean/WorkDetail.kt
+++ baseLibrary/src/main/java/cn/com/base/simple/bean/WorkDetail.kt
@@ -1,5 +1,8 @@
 package cn.com.base.simple.bean
 
+import android.graphics.drawable.Drawable
+import android.support.v4.content.ContextCompat
+import cn.com.base.LibApplication
 import cn.com.base.R
 import cn.com.base.util.DateUtil
 
@@ -15,10 +18,7 @@ class WorkDetail {
     var categoryName: String = ""
     var comments: Int = 0
     var cover: String = ""
-    var createtime: String = ""
-        get() {
-            return DateUtil.getTimeDescrition(createtime.toLong())
-        }
+
     var duration: Int = 0
     var favorites: Int = 0
     var favors: Int = 0
@@ -26,21 +26,7 @@ class WorkDetail {
     var hid: Int = 0
     var id: Int = 0
     var imageList: List<String> = listOf()
-    var isFavor: Int = 0
-        get() {
-            if (field == 0)
-                return R.mipmap.ic_test_flower_unselected
-            else
-                return R.mipmap.ic_test_flower_selected
-        }
     var isFavorite: Int = 0
-    var isFollow: Int = 0
-        get() {
-            if (field == 0)
-                return R.mipmap.ic_test_collect_unselected
-            else
-                return R.mipmap.ic_test_collect_selected
-        }
     var isRecommend: Int = 0
     var name: String = ""
     var path: String = ""
@@ -51,4 +37,36 @@ class WorkDetail {
     var type: Int = 0
     var uid: Int = 0
     var width: Int = 0
+
+    /**
+     * 设置是否鲜花
+     */
+    var isFavor: Int? = 0
+    var isFavorBg: Drawable? = null
+        get() {
+            if (isFavor == 0)
+                return ContextCompat.getDrawable(LibApplication.application, R.mipmap.ic_test_flower_unselected)
+            else
+                return ContextCompat.getDrawable(LibApplication.application, R.mipmap.ic_test_flower_selected)
+        }
+    /**
+     * 设置是否收藏
+     */
+    var isFollow: Int? = 0
+    var isFollowBg: Drawable? = null
+        get() {
+            if (isFollow == 0)
+                return ContextCompat.getDrawable(LibApplication.application, R.mipmap.ic_test_collect_unselected)
+            else
+                return ContextCompat.getDrawable(LibApplication.application, R.mipmap.ic_test_collect_selected)
+        }
+
+    /**
+     * 设置时间
+     */
+    var createtime: Long = 0
+    var mCreatetime: String = ""
+        get() {
+            return DateUtil.getTimeDescrition(createtime)
+        }
 }
